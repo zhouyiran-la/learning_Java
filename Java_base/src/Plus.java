92b7991aa2526f4cf3dd2c3876c9dcdb6c69774d
zhouyiran <vegetablezhouya@gmail.com>
2023-07-15 00:19:18 +0800

    Create Plus.java file.

---
+++ Java_base/src/Plus.java
@@ -0,0 +1,8 @@
+public class Plus {
+    public static void main(String[] args) {
+        //加号中有一方为字符串，做拼接运算
+        System.out.println(100 + 98);//198
+        System.out.println("100" + 98);//10098
+        System.out.println(100 + 3 + "Hello");//1003Hello
+    }
+}
