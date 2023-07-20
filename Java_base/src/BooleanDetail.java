a4582aee63202c90804b0b2e2127e7ad53f12a68
zhouyiran <zhouyiran_la@163.com>
2023-07-20 16:38:47 +0800

    Add .\src\BooleanDetail.java file.

---
+++ Java_base/src/BooleanDetail.java
@@ -0,0 +1,12 @@
+//简单展示boolean变量类型使用
+//boolean[1]
+public class BooleanDetail {
+    public static void main(String[] args) {
+        boolean isPass = false;
+        if(isPass == true) {
+            System.out.println("考试通过");
+        } else {
+            System.out.println("考试为未通过");
+        }
+    }
+}
