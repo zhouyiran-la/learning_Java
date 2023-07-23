8b77bce61c1cd04ab2271b5b5fbb595ccd516925
zhouyiran <zhouyiran_la@163.com>
2023-07-23 16:54:01 +0800

    Add .\Java_base\chap02\src\Homework.java file.

---
+++ Java_base/chap02/src/Homework.java
@@ -0,0 +1,12 @@
+public class Homework {
+    public static void main(String[] args) {
+        
+        String name = "周依然";
+        byte age = 22;
+        double score = 89.88;
+        char gender = '女';
+        String hobby = "读书看报";
+
+        System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n" + name + "\t" + age + "\t" + score + "\t" + gender + "\t"+ hobby);
+    }
+}
