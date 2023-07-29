cb4860a901f9896762558e9081bbd6024c77fd1f
zhouyiran <zhouyiran_la@163.com>
2023-07-29 20:59:59 +0800

    Add .\Java_base\chap04\src\While.java & WhileExercise.java file.

---
+++ Java_base/chap04/src/WhileExercise.java
@@ -0,0 +1,24 @@
+
+public class WhileExercise {
+    public static void main(String[] args) {
+        // 01
+        int i = 1;
+        while(i <= 100) {
+            if(i % 3 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    
+    System.out.println("====================");   
+
+        // 02 
+        int j = 40;
+        while(j <= 200) {
+            if(j % 2 == 0) {
+                System.out.println(j);
+            }
+            j++;
+        }
+    }
+}
