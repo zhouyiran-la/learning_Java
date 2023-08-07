e50e779abec3fd6097dbba405e361b1e7d8fb0a0
zhouyiran <zhouyiran_la@163.com>
2023-08-07 22:44:04 +0800

    Add Method02.java & Method.class & Mytools.class file in chap06.

---
+++ Java_base/chap06/src/Method02.java
@@ -0,0 +1,26 @@
+// 遍历一个数组， 输出数组的各个元素值
+public class Method02 {
+    public static void main(String[] args) {
+
+        MyTools tool = new MyTools();
+        int[][] map = {{0, 0, 1}, {1, 1, 1}, {1, 1, 3}};
+        // 提高代码复用性
+        tool.PrintArr(map);
+        System.out.println("==========");
+        tool.PrintArr(map);
+    }
+    
+}
+
+class MyTools {
+
+    public void PrintArr(int[][] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
