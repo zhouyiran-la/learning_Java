81feaca44e1080bd6b33828a12a8b4f696db588e
zhouyiran <zhouyiran_la@163.com>
2023-08-20 23:48:08 +0800

    Create testpro Java project.

---
+++ use_IDEA/testpro/src/ArrayTest.java
@@ -0,0 +1,33 @@
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        Mytools tool = new Mytools();
+        int[] arr = {10, -1, 8, 0, 34};
+        tool.bubble(arr);
+        // 输出排序后的arr
+        tool.print(arr);
+    }
+}
+
+class Mytools {
+
+    // 冒泡排序
+    public void bubble(int[] arr) {
+        int tmp;
+        for(int i = 0; i < arr.length - 1; i++) {
+            for(int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+    }
+
+    public void print(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
