e0031d73a9603a48437e755eb8ea4e551b78b97a
zhouyiran <zhouyiran_la@163.com>
2023-08-23 11:07:00 +0800

    Add ArrayTest.java file in testpro proiect.

---
+++ use_IDEA/testpro/src/TestTemplate.java
@@ -0,0 +1,32 @@
+/*
+模版/自定义模版
+file-> settings -> editor -> Live templates
+ */
+public class TestTemplate {
+    // main模版
+    public static void main(String[] args) {
+        // sout模版
+        System.out.println();
+
+        // fori模版
+//        for (int i = 0; i < 10; i++) {
+//
+//        }
+        // ifn模版
+//        if (args == null) {
+//
+//        }
+        // inn 模版
+//        if (args != null) {
+//
+//        }
+
+        // else-if 模版
+//        if (args == null) {
+//
+//        } else if () {
+//
+//        }
+    }
+
+}
